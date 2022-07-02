fd68a6128ac146a844da486d863bf660389f15f8
Russell Blickhan <rwblickhan@gmail.com>
2022-07-02 14:16:26 -0700

    Fix installing wrong git-delta

--- install.sh
+++ install.sh
@@ -9,13 +9,9 @@ sudo apt-get install neovim -y
 sudo apt-get install ripgrep -y
 sudo apt-get install fd-find -y
 sudo apt-get install fzf -y
-sudo apt-get install git-extras -y
 
 echo "Symlinking fd..."
 ln -s $(which fdfind) ~/.local/bin/fd
-
-echo "Symlinking delt..."
-ln -s $(which git-delta) ~/.local/bin/delta
 
 echo "Installing Powerline fonts..."
 sudo apt-get install powerline fonts-powerline -y
