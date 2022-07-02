6ab49a8e626e65d9f2058a1848a0c420353f06e7
Russell Blickhan <rwblickhan@gmail.com>
2022-07-02 11:01:13 -0700

    Create minimal .zshrc

--- install.sh
+++ install.sh
@@ -4,20 +4,11 @@ create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+    echo "Symlinking .zshrc..."
+    ln -s $script_dir/.zshrc.minimal ~/.zshrc
 
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
-
-    # Copy .config folder into appropriate place
-    echo "Copying .config folder"
-    cp -r .config ~/.config
+    echo "Copying starship config..."
+    cp $script_dir/.config/starship.toml ~/.config/starship.toml
 }
 
 create_symlinks
@@ -26,12 +17,8 @@ echo "Installing command-line tools..."
 sudo apt-get update
 sudo apt-get install neovim -y
 sudo apt-get install ripgrep -y
-sudo apt-get install exa -y
-sudo apt-get install bat -y
 sudo apt-get install fd-find -y
 sudo apt-get install fzf -y
-sudo apt-get install zoxide -y
-sudo apt-get install cargo -y
 
 echo "Installing Powerline fonts..."
 sudo apt-get install powerline fonts-powerline -y
@@ -40,6 +27,3 @@ echo "Installing starship theme..."
 curl -sS https://starship.rs/install.sh >> starship_install.sh
 chmod +x starship_install.sh
 ./starship_install.sh -y
-
-echo "Installing git-delta..."
-cargo install git-delta
