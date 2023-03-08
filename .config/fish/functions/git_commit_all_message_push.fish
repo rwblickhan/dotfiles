1fc718fd7b7be30c1f6cbe6f38313770729600e2
Russell Blickhan <rwblickhan@gmail.com>
2023-03-08 10:59:21 -0800

    Update git alises

--- .config/fish/functions/git_commit_all_message_push.fish
+++ .config/fish/functions/git_commit_all_message_push.fish
@@ -1,4 +1,3 @@
 function git_commit_all_message_push
-    read -p "set_color green; echo 'Commit message?'; set_color normal; echo '> '" message
-    git add -A && git commit -m "$message" && git push
+    git_commit_all_message $argv && git push
 end
