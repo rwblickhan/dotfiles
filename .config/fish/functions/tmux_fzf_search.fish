e737b0a232023f8afe09f748ee6f50ff1c3a6e20
Russell Blickhan <rwblickhan@gmail.com>
2023-04-12 13:02:01 -0700

    Add tmux_fzf_search

---
+++ .config/fish/functions/tmux_fzf_search.fish
@@ -0,0 +1,19 @@
+function tmux_fzf_search
+    set options urls\nfiles\ndigits\nhashes\nparens\nsingle-quotes
+    set selection (echo $options | fzf)
+    tmux copy-mode
+    switch $selection
+        case urls
+            tmux send-keys -X search-backward '(https?://|git@|git://|ssh://|ftp://|file:///)[[:alnum:]?=%/_.:,;~@!#$&()*+-]*'
+        case files
+            tmux send-keys -X search-backward '(^|^\.|[[:space:]]|[[:space:]]\.|[[:space:]]\.\.|^\.\.)[[:alnum:]~_-]*/[][[:alnum:]_.#$%&+=/@-]*'
+        case digits
+            tmux send-keys -X search-backward '[[:digit:]]+'
+        case hashes
+            tmux send-keys -X search-backward '\b([0-9a-f]{7,40}|[[:alnum:]]{52}|[0-9a-f]{64})\b'
+        case parens
+            tmux send-keys -X search-backward '"[^"]*"'
+        case single-quotes
+            tmux send-keys -X search-backward '\'[^\']*\''
+    end
+end
