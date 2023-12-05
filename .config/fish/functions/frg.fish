dcbd64f1b5394718fe43a52e2799a2ab88944659
Russell Blickhan <rwblickhan@gmail.com>
2023-12-04 23:11:39 -0800

    Add source link

--- .config/fish/functions/frg.fish
+++ .config/fish/functions/frg.fish
@@ -1,9 +1,10 @@
 function frg --description 'rg tui built with fzf and bat'
-        rg --ignore-case --color=always --line-number --no-heading "$argv" |
-            fzf --ansi \
-                --color 'hl:-1:underline,hl+:-1:underline:reverse' \
-                --delimiter ':' \
-                --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
-                --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-                --bind "enter:become($EDITOR +{2} {1})"
+    # Source: https://news.ycombinator.com/item?id=38475537
+    rg --ignore-case --color=always --line-number --no-heading "$argv" |
+        fzf --ansi \
+            --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+            --delimiter ':' \
+            --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+            --bind "enter:become($EDITOR +{2} {1})"
 end
