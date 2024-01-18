c47c9a8c9eec514b2cedffea92d4f1d78ee78b86
Russell Blickhan <rwblickhan@gmail.com>
2024-01-17 17:42:52 -0800

    Add preview to git r

--- .config/fish/functions/git_fzf_restore.fish
+++ .config/fish/functions/git_fzf_restore.fish
@@ -1,7 +1,7 @@
 function git_fzf_restore
     set files (git diff --name-only)
     if test (count $files) -gt 0
-        printf '%s\n' $files | fzf -m | xargs git restore
+        printf '%s\n' $files | fzf -m --preview="echo {} | xargs git diff | delta" | xargs git restore
     else
         echo "No unstaged changes found"
     end
--- .config/fish/functions/git_fzf_restore_staged.fish
+++ .config/fish/functions/git_fzf_restore_staged.fish
@@ -1,7 +1,7 @@
 function git_fzf_restore_staged
     set files (git diff --name-only --cached)
     if test (count $files) -gt 0
-        printf '%s\n' $files | fzf -m | xargs git restore --staged
+        printf '%s\n' $files | fzf -m --preview="echo {} | xargs git diff HEAD | delta" | xargs git restore --staged
     else
         echo "No staged changes found"
     end
