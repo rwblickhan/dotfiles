801bbdeff1963010eab4f22db082c04a44a50450
Russell Blickhan <rwblickhan@gmail.com>
2024-03-25 16:49:23 -0700

    Add git dts

--- .config/fish/functions/git_fzf_diff_file_staged.fish
+++ .config/fish/functions/git_fzf_diff_file_staged.fish
@@ -1,7 +1,7 @@
 function git_fzf_diff_file_staged
     set files (git diff --name-only HEAD)
     if test (count $files) -gt 0
-        printf '%s\n' $files | fzf -m --preview="echo {} | xargs git diff HEAD | delta" | xargs git diff HEAD
+        printf '%s\n' $files | fzf -m --preview="echo {} | xargs git diff --staged | delta" | xargs git diff HEAD
     else
         echo "No changes found"
     end
