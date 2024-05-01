736415a293b8990d18cf7ff88093d49b7e1f31ff
Russell Blickhan <rwblickhan@gmail.com>
2024-04-30 22:53:58 -0700

    Update nvim config - Remove vim-sensible, vim-commentary, and targets.vim plugins - Add vim-fugitive, fzf, and fzf.vim plugins - Set up mini.nvim plugins for basic settings, ai, commenting, completion, pairs, and jump - Set leader key to backslash - Remove ignorecase and smartcase options - Remove number option - This commit message was written by a large language model

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,22 +2,27 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
-Plug('tpope/vim-sensible')
-Plug('tpope/vim-commentary')
 Plug('tpope/vim-surround')
 Plug('tpope/vim-repeat')
 Plug('tpope/vim-speeddating')
+Plug('tpope/vim-fugitive')
 Plug('bkad/CamelCaseMotion')
 Plug('machakann/vim-swap')
-Plug('wellle/targets.vim')
 Plug('christoomey/vim-tmux-navigator')
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 Plug('Shatur/neovim-ayu')
+Plug('junegunn/fzf', { ['do'] = 'fzf#install()'  })
+Plug('junegunn/fzf.vim')
 Plug('echasnovski/mini.nvim')
 vim.call('plug#end')
 
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
+require('mini.basics').setup()
+require('mini.ai').setup({})
+require('mini.comment').setup({})
+require('mini.completion').setup({})
+require('mini.pairs').setup()
+require('mini.jump').setup()
+
 vim.keymap.set('n', 'H', '^', { noremap = true })
 vim.keymap.set('n', 'L', '$', { noremap = true })
 vim.keymap.set('v', 'H', '^', { noremap = true })
@@ -25,16 +30,14 @@ vim.keymap.set('v', 'L', '$', { noremap = true })
 vim.keymap.set('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true })
 vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
 
+-- Use \ as leader
+vim.g.mapleader = '\\'
 -- Use /g by default in regex substitution
 vim.opt.gdefault = true
--- Use smarter regex case matching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 -- Use 2 spaces for tabs
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 -- Show relative line numbers
-vim.opt.number = true
 vim.opt.relativenumber = true
 -- Use system clipboard
 vim.opt.clipboard:append('unnamedplus')
@@ -55,10 +58,6 @@ require('nvim-treesitter.configs').setup {
     additional_vim_regex_highlighting = false,
   },
 }
-
-require('mini.completion').setup({})
-require('mini.pairs').setup()
-require('mini.jump').setup()
 
 -- Use ayu dark
 require('ayu').setup({
