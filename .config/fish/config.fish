9203f4c405a7ee0acd331890d24d7b1f2024bd1b
Russell Blickhan <rwblickhan@gmail.com>
2024-08-13 16:24:39 -0700

    Add git-spice shell completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,7 +33,6 @@ alias diff delta
 if status is-interactive
     zoxide init fish --cmd cd | source
     starship init fish | source
-    eval "$(gs shell completion fish)"
 
     abbr -a g git
     abbr -a gsp git stash pop
