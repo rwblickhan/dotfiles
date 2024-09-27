bb3f26eaac3a158a8c8457efd6fd9cd6092bd91c
Russell Blickhan <rwblickhan@gmail.com>
2024-09-27 13:22:33 -0700

    Add log.sh

---
+++ log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Log
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author rwblickhan
+# @raycast.authorURL https://raycast.com/rwblickhan
+
+cd ~/Developer/astro-rwblickhan.org && just log
