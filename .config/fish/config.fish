9835705a613f97e4ca537b84734966152e929bd8
Russell Blickhan <rwblickhan@gmail.com>
2025-01-03 11:44:03 -0800

    Fix gh pv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,7 +47,7 @@ if status is-interactive
     abbr -a f rfv
     abbr -a z zed
     abbr -a bbic "brew bundle install --cleanup --file=~/.config/Brewfile --no-lock && brew upgrade"
-    abbr -a --command gh pv 'pr view -v'
+    abbr -a --command gh pv 'pr view -w'
     abbr -a --command gh ps 'pr status'
     abbr -a --command gh pm 'pr merge'
 end
