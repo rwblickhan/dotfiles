b6c045fb5e60c0a58040c2495cd134088b260212
Russell Blickhan <rwblickhan@gmail.com>
2025-02-27 11:49:50 -0800

    Remove unnecessary bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,9 +33,6 @@ alias find fd
 alias diff delta
 alias man "BAT_THEME='Monokai Extended' batman"
 
-bind alt-backspace backward-kill-word
-bind ctrl-backspace backward-kill-token
-
 if status is-interactive
     zoxide init fish --cmd cd | source
     starship init fish | source
