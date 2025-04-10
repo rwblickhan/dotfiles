96d7782bb56986e6c0c6e03f60fcc406f01cf4d7
Russell Blickhan <rwblickhan@gmail.com>
2025-04-10 10:48:08 -0700

    Add markdown link config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,70 @@
 hs.loadSpoon("LeftRightHotkey")
 spoon.LeftRightHotkey:start()
 
+local clipboardHistory = {}
+local maxClipboardHistory = 10
+
+-- Insert clipboard changes into local memory
+local clipboardWatcher = hs.pasteboard.watcher.new(function(contents)
+    if contents then
+        table.insert(clipboardHistory, 1, contents)
+        if #clipboardHistory > maxClipboardHistory then
+            table.remove(clipboardHistory)
+        end
+    end
+end)
+clipboardWatcher:start()
+
+local function isURL(text)
+    if not text then return false end
+    local pattern = "^https?://[%w-_%.%?%.:/%+=&%%]+$"
+    return string.match(text, pattern) ~= nil
+end
+
+local function findURLAndText()
+    local recentURL = nil
+    local recentText = nil
+    
+    for _, item in ipairs(clipboardHistory) do
+        if isURL(item) and not recentURL then
+            recentURL = item
+        elseif not isURL(item) and not recentText then
+            recentText = item
+        end
+        
+        if recentURL and recentText then
+            break
+        end
+    end
+    
+    return recentText, recentURL
+end
+
+local function createMarkdownLink()
+    local text, url = findURLAndText()
+    
+    if not text then
+        hs.alert.show("No text found in clipboard history")
+        return
+    end
+    
+    if not url then
+        hs.alert.show("No URL found in clipboard history")
+        return
+    end
+    
+    local markdownLink = string.format("[%s](%s)", text, url)
+    hs.eventtap.keyStrokes(markdownLink)
+end
+
 local function typeCheckbox()
     hs.eventtap.keyStrokes("- [x]")
 end
 
 spoon.LeftRightHotkey:bind({"rCtrl"}, "x", typeCheckbox)
 spoon.LeftRightHotkey:bind({"rOpt"}, "x", typeCheckbox)
-
 print("Markdown checkbox config created!")
+
+spoon.LeftRightHotkey:bind({"rCtrl"}, "l", createMarkdownLink)
+spoon.LeftRightHotkey:bind({"rOpt"}, "l", createMarkdownLink)
+print("Markdown link config created!")
