a9b8b2dbe4ce7bff0689cc3adf222896e48a43d8
Russell Blickhan <rwblickhan@gmail.com>
2025-05-15 21:10:26 -0700

    Various config updates

--- .config/fish/functions/backup.fish
+++ .config/fish/functions/backup.fish
@@ -1,4 +1,4 @@
 function backup
-  source ~/utils/source.fish
-  restic -r s3:https://d68842214bc7eab6283e7ef8876b12e6.r2.cloudflarestorage.com/backups backup --verbose Movies/ Calibre\ Library/ Developer/ Documents/ Desktop/
+  source ~/utils/secrets.fish
+  restic -r s3:https://d68842214bc7eab6283e7ef8876b12e6.r2.cloudflarestorage.com/backups backup --verbose ~/Movies/ ~/Calibre\ Library/ ~/Developer/ ~/Documents/ ~/Desktop/
 end
