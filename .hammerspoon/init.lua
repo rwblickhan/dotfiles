1a39828170ef151ca35f37de49fc549ed5a5ced2
Russell Blickhan <rwblickhan@gmail.com>
2025-09-13 13:09:38 -0700

    Add hammerspoon arrow key nav config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -8,3 +8,15 @@ end
 
 spoon.LeftRightHotkey:bind({ "rOpt" }, "x", typeCheckbox)
 print("Markdown checkbox config created!")
+
+local function sendDownArrow()
+    hs.eventtap.keyStroke({}, "down")
+end
+
+local function sendUpArrow()
+    hs.eventtap.keyStroke({}, "up")
+end
+
+hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "j", sendDownArrow)
+hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "k", sendUpArrow)
+print("Arrow key navigation config created!")
