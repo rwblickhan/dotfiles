f74600d22da700a36b16bbde5faa8cf7de505a98
Russell Blickhan <russell@descript.com>
2025-10-28 15:06:12 -0700

    Remove descript-specific configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,13 +65,4 @@ if status is-interactive
 
     abbr -a exp expense
     abbr -a bbic "brew bundle install --file=~/.config/Brewfile.minimal && brew upgrade"
-
-    # Descript-specific
-    abbr -a api 'code ~/descript/services/api'
-    abbr -a client 'code ~/descript/pkg-js/client'
-    abbr -a web 'code ~/descript/apps/web'
-    abbr -a sync 'git checkout main && gt sync && gt ss -u'
-    abbr -a start-web 'pnpm run start-web-secure'
-    abbr -a start-e2e 'pnpm run start-web-e2e'
-    abbr -a start-exposed 'pnpm run start-web-secure-exposed'
 end
