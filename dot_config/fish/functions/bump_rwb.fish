579435b9e9b5709f4d872373563a35902938c401
Russell Blickhan <rwblickhan@gmail.com>
2026-03-04 13:51:21 -0800

    Update bump_rwb to use jj instead

--- dot_config/fish/functions/bump_rwb.fish
+++ dot_config/fish/functions/bump_rwb.fish
@@ -1,3 +1,8 @@
 function bump_rwb
-  cd ~/Developer/astro-rwblickhan.org && git diff-index --quiet HEAD && echo "No changes!" || git camp "Log"
+  cd ~/Developer/astro-rwblickhan.org
+  if jj diff | string match -qr '.'
+    jj describe -m "Log"; and jj bookmark set main; and jj git push
+  else
+    echo "No changes!"
+  end
 end
