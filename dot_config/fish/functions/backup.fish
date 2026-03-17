392033027959811663a2ca50951a94a8b63b1e87
Russell Blickhan <rwb@rwblickhan.org>
2026-03-17 09:47:24 -0700

    Fix setting r2b endpoint in backups

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -1,5 +1,5 @@
 function backup
     set -lx AWS_ACCESS_KEY_ID (op read "op://Private/Cloudflare Backups/access_key_id")
     set -lx AWS_SECRET_ACCESS_KEY (op read "op://Private/Cloudflare Backups/secret_access_key")
-    restic -r s3:(r2b_endpoint)/backups backup --verbose ~/Movies/ ~/Calibre\ Library/ ~/Developer/ ~/Documents/ ~/Desktop/
+    restic -r s3:(r2b_endpoint) backup --verbose ~/Movies/ ~/Calibre\ Library/ ~/Developer/ ~/Documents/ ~/Desktop/
 end
