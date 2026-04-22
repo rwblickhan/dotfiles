9e3dcc1a5dfe68c7eb72199be62e26da7efdef57
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-22 16:03:12 -0700

    Install jj correctly in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
-sudo apt install fish jj -y
+sudo apt install fish install build-essential -y
+cargo install --locked --bin jj jj-cli
