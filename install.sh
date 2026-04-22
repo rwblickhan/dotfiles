4d5d6ff3f42c395d458a953fc5948263e8c45d8d
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-22 15:29:17 -0700

    Auto-appove apt installations in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
-sudo apt install fish
-sudo apt install jj
+sudo apt install fish jj -y
