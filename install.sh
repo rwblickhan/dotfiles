c58463691332421b6585c95b7aecfa43f59770fb
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 09:37:51 -0700

    [install.sh] Install git-delta

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
-sudo apt install fish -y
+sudo apt install fish git-delta -y
 # Install jj
 curl -L https://github.com/jj-vcs/jj/releases/download/v0.40.0/jj-v0.40.0-aarch64-unknown-linux-musl.tar.gz | tar -xz -C $HOME/.local/bin ./jj
