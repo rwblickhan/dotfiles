8713724b8825d03414d0e570781a26868b69ec1f
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 09:24:10 -0700

    [install.sh] Install jj directly from github

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
 sudo apt install fish -y
-# Install jj via cargo-binstall
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall --strategies crate-meta-data jj-cli
+# Install jj
+curl -L https://github.com/jj-vcs/jj/releases/download/v0.40.0/jj-v0.40.0-aarch64-unknown-linux-musl.tar.gz | tar -xz -C $HOME/.local/bin jj
