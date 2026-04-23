767802de3c346f82138916cd773b4708cb4919e7
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 09:11:13 -0700

    [install.sh] Use sh -y on rustup installation

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
 sudo apt install fish -y
 # Install jj via cargo-binstall
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall --strategies crate-meta-data jj-cli
