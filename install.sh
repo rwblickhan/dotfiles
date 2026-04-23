3ffaa777432357398b001f7f197ee76ab9367ae3
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 09:29:57 -0700

    [install.sh] Maybe fix jj installation

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
 sudo apt install fish -y
 # Install jj
-curl -L https://github.com/jj-vcs/jj/releases/download/v0.40.0/jj-v0.40.0-aarch64-unknown-linux-musl.tar.gz | tar -xz -C $HOME/.local/bin jj
+curl -L https://github.com/jj-vcs/jj/releases/download/v0.40.0/jj-v0.40.0-aarch64-unknown-linux-musl.tar.gz | tar -xz -C $HOME/.local/bin ./jj
