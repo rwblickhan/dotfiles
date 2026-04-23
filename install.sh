ada3894c91af33c82f3403ed7ecdff00ade1d8c6
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 08:54:26 -0700

    [install.sh] Use cargo-binstall to install jj instead

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b $HOME/.local/bin
 chezmoi init --apply https://github.com/rwblickhan/chezmoi.git
-sudo apt install fish install build-essential -y
-cargo install --locked --bin jj jj-cli
+sudo apt install fish -y
+# Install jj
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+cargo binstall --strategies crate-meta-data jj-cli
