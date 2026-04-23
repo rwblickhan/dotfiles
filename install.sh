181f89c9d74915c8035b860617a221e86d88e542
Russell Blickhan <russell.blickhan@vanta.com>
2026-04-23 10:26:20 -0700

    [install.sh] Switch back to x86 for helix

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ sudo apt install fish git-delta fd-find -y
 
 # Install Helix from GitHub releases
 HX_VERSION=$(curl -fsLS https://api.github.com/repos/helix-editor/helix/releases/latest | grep '"tag_name"' | sed 's/.*"tag_name": "\(.*\)".*/\1/')
-HX_TARBALL="helix-${HX_VERSION}-aarch64-linux.tar.xz"
+HX_TARBALL="helix-${HX_VERSION}-x86_64-linux.tar.xz"
 curl -fsLS "https://github.com/helix-editor/helix/releases/download/${HX_VERSION}/${HX_TARBALL}" -o "/tmp/${HX_TARBALL}"
 tar -xf "/tmp/${HX_TARBALL}" -C /tmp
-mv "/tmp/helix-${HX_VERSION}-aarch64-linux/hx" ~/.local/bin/hx
+mv "/tmp/helix-${HX_VERSION}-x86_64-linux/hx" ~/.local/bin/hx
 mkdir -p ~/.config/helix
-cp -r "/tmp/helix-${HX_VERSION}-aarch64-linux/runtime" ~/.config/helix/runtime
-rm -rf "/tmp/${HX_TARBALL}" "/tmp/helix-${HX_VERSION}-aarch64-linux"
+cp -r "/tmp/helix-${HX_VERSION}-x86_64-linux/runtime" ~/.config/helix/runtime
+rm -rf "/tmp/${HX_TARBALL}" "/tmp/helix-${HX_VERSION}-x86_64-linux"
 
 # Alias fdfind to fd
 ln -s $(which fdfind) ~/.local/bin/fd
