04e06b9d9622ccfdd8a28e28adbc7724ec7c8eae
Russell Blickhan <rwb@rwblickhan.org>
2026-04-27 18:45:27 -0700

    Remove sync-terminfo

--- dot_config/fish/functions/sync-terminfo.fish
+++
@@ -1,7 +0,0 @@
-function sync-terminfo --description "Sync xterm-ghostty terminfo to a remote server" --argument-names server
-    if test -z "$server"
-        echo "Usage: sync-terminfo YOUR-SERVER"
-        return 1
-    end
-    infocmp -x xterm-ghostty | ssh $server -- tic -x -
-end
