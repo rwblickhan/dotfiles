1d865aba1e67dcb2d8c27a64e42a2688d9a2b542
Russell Blickhan <rwb@rwblickhan.org>
2026-04-29 21:02:54 -0700

    Only backup personal pictures

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -1,4 +1,4 @@
 function backup
     load_backups_credentials
-    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/Personal/ ~/Music/Personal/ ~/Pictures/
+    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/Personal/ ~/Music/Personal/ ~/Pictures/Personal/
 end
