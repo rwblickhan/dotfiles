314dbe993018e99a0a46744a49904443810b3a3a
Russell Blickhan <rwb@rwblickhan.org>
2026-04-29 19:30:04 -0700

    Scope backups to personal folders only

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -1,5 +1,5 @@
 function backup
     set -lx AWS_ACCESS_KEY_ID (op read "op://Private/Cloudflare Backups/access_key_id")
     set -lx AWS_SECRET_ACCESS_KEY (op read "op://Private/Cloudflare Backups/secret_access_key")
-    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/ ~/Music/ ~/Pictures/
+    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/Personal/ ~/Music/Personal/ ~/Pictures/
 end
