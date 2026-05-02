96d1b17736fe85dd053ed7b7abdde6aede5cd888
Russell Blickhan <rwb@rwblickhan.org>
2026-05-01 20:24:55 -0700

    Add nvim abbr and remove s abbr

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -65,11 +65,11 @@ if status is-interactive
     abbr -a h hx
     abbr -a j jj
     abbr -a m mise
+    abbr -a n nvim
     abbr -a o open
     abbr -a p pnpm
     abbr -a r 'mise run'
     abbr -a z zmx
-    abbr --set-cursor -a s 'git commit -m "%" && git push'
 
     abbr -a nw new_workspace
     abbr -a exp expense
