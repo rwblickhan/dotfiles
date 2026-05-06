16a7c6af8c97891f072d63e41ebad1ca7cfcfff7
Russell Blickhan <russell.blickhan@vanta.com>
2026-05-06 10:46:21 -0700

    Add ona_layout.sh for raycast control

---
+++ utils/executable_ona_layout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ona layout
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author rwblickhan
+# @raycast.authorURL https://raycast.com/rwblickhan
+
+fish -c ona_layout
