d27c546ef2c519b9a671ab6e214fc14827d54e7f
Russell Blickhan <russell.blickhan@vanta.com>
2026-05-12 10:55:17 -0700

    [install.sh] Avoid persisting homebrew shellenv

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b "$HOME/.local/bin"
 # Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Add brew to PATH for this session and persist to .bashrc (Linux install path)
+# Add brew to PATH for this session only
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-printf '\neval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"\n' >> "$HOME/.bashrc"
 printf '\nexec fish\n' >> "$HOME/.bashrc"
 printf '\n# Fix Zed remote extension permissions (root-owned .tmp dirs block extension install)\nfind ~/.local/share/zed/remote_extensions -user root -exec sudo chown -R vscode:vscode {} + 2>/dev/null\n' >> "$HOME/.bashrc"
 
@@ -28,3 +27,8 @@ brew install \
     jj \
     typescript-language-server \
     neurosnap/tap/zmx
+
+# Symlink all brew-installed binaries to ~/.local/bin so brew doesn't need to be on PATH
+for bin in /home/linuxbrew/.linuxbrew/bin/*; do
+    ln -sf "$bin" "$HOME/.local/bin/$(basename "$bin")"
+done
