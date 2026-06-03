30d4aaaf45960aef72f67a7265318b689f2c60d5
Russell Blickhan <rwb@rwblickhan.org>
2026-06-03 13:41:56 -0700

    Fix jump2d config to match helix

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,7 +14,7 @@ require('mini.comment').setup({})
 require('mini.git').setup({})
 require('mini.jump').setup()
 require('mini.jump2d').setup({
-  mappings = { start_jumping = 'gw' },
+  mappings = { start_jumping = '<CR>' },
 })
 require('mini.operators').setup({ replace = { prefix = "R" }})
 require('mini.surround').setup({
