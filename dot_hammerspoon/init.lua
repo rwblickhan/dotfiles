9a67a994110b7d3499579cc3eee5d2f887b0c7db
Russell Blickhan <rwb@rwblickhan.org>
2026-07-20 11:20:26 -0700

    Add ctrl-t Task in Things shortcut for Drafts

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -484,6 +484,7 @@ end)
 bindConditionalHotkey({ "ctrl", "cmd" }, "l", isDraftsFocused, function() selectMenuItem("Drafts", "Link Mode") end)
 bindConditionalHotkey({ "ctrl", "cmd" }, "k", isDraftsFocused, function() selectMenuItem("Drafts", "Kebab Case") end)
 bindConditionalHotkey({ "ctrl", "cmd" }, "t", isDraftsFocused, function() selectMenuItem("Drafts", "Title Case") end)
+bindConditionalHotkey({ "ctrl" }, "t", isDraftsFocused, function() selectMenuItem("Drafts", "Task in Things") end)
 
 -- Chrome-specific hotkeys
 bindConditionalHotkey({ "cmd" }, "d", isChromeFocused, openChromeTabInSplitView)
