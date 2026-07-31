fdd417efe7802932a7dcf263afa6133d3e7a2107
Russell Blickhan <rwb@rwblickhan.org>
2026-07-30 21:15:49 -0700

    Remove fix_markdown and bump_last_updated_date

--- dot_config/fish/functions/bump_last_updated_date.fish
+++
@@ -1,3 +0,0 @@
-function bump_last_updated_date
-    sd 'lastUpdatedDate: \d\d\d\d-\d\d-\d\d' "lastUpdatedDate: "(date +%Y-%m-%d)
-end
--- dot_config/fish/functions/fix_markdown.fish
+++
@@ -1,8 +0,0 @@
-function fix_markdown
-    set -l tmpfile (mktemp /tmp/fix_markdown_XXXXXX.md)
-    cat > $tmpfile
-    markdownlint $tmpfile --fix 2>/dev/null; or true
-    sd 'lastUpdatedDate: \d\d\d\d-\d\d-\d\d' "lastUpdatedDate: "(date +%Y-%m-%d) $tmpfile
-    cat $tmpfile
-    rm $tmpfile
-end
