fec07d44451a8e62064de67d77b3dca69b2bfcb1
Russell Blickhan <rwb@rwblickhan.org>
2026-07-30 17:10:36 -0700

    Fix path for bump_rwb

--- dot_config/fish/functions/bump_rwb.fish
+++ dot_config/fish/functions/bump_rwb.fish
@@ -1,18 +1,18 @@
 function bump_rwb
-  cd ~/Developer/astro-rwblickhan.org/main
-  if jj log -r "@" --no-graph -T 'description' | string match -qr '.'
-    echo "Error: current revision is already described"
-    return 1
-  end
-  set -l parent_id (jj log -r "@-" --no-graph -T 'commit_id')
-  set -l main_id (jj log -r "main" --no-graph -T 'commit_id')
-  if test "$parent_id" != "$main_id"
-    echo "Error: parent of current revision is not the main bookmark"
-    return 1
-  end
-  if jj diff --name-only | string match -qr '.'
-    jj describe -m "Log"; and jj fix; and jj bookmark advance; and jj-hp push
-  else
-    echo "No changes!"
-  end
+    cd ~/Developer/astro-rwblickhan.org
+    if jj log -r "@" --no-graph -T description | string match -qr '.'
+        echo "Error: current revision is already described"
+        return 1
+    end
+    set -l parent_id (jj log -r "@-" --no-graph -T 'commit_id')
+    set -l main_id (jj log -r "main" --no-graph -T 'commit_id')
+    if test "$parent_id" != "$main_id"
+        echo "Error: parent of current revision is not the main bookmark"
+        return 1
+    end
+    if jj diff --name-only | string match -qr '.'
+        jj describe -m Log; and jj fix; and jj bookmark advance; and jj-hp push
+    else
+        echo "No changes!"
+    end
 end
