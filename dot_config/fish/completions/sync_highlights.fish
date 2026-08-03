5873f846aa61536c3a96a7b217c8eec3ec7ad1f9
Russell Blickhan <rwb@rwblickhan.org>
2026-08-02 17:18:09 -0700

    Add sync_highlights fish completions

---
+++ dot_config/fish/completions/sync_highlights.fish
@@ -0,0 +1,5 @@
+complete -c sync_highlights -s t -l target -d 'Target directory to copy new files into' -r -F
+complete -c sync_highlights -s d -l dry-run -d 'Dry run - show what would be copied without copying'
+complete -c sync_highlights -s v -l verbose -d 'Print verbose output including import progress'
+complete -c sync_highlights -s h -l help -d 'Print help'
+complete -c sync_highlights -s V -l version -d 'Print version'
