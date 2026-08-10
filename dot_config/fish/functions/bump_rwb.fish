5710ea5d07fcd7849f701f2094cd57da938ca82a
Russell Blickhan <rwb@rwblickhan.org>
2026-08-10 14:29:42 -0700

    Move last-updated-date to jj fix

--- dot_config/fish/functions/bump_rwb.fish
+++ dot_config/fish/functions/bump_rwb.fish
@@ -11,7 +11,7 @@ function bump_rwb
         return 1
     end
     if jj diff --name-only | string match -qr '.'
-        jj describe -m Log; and jj run-hk; and jj bookmark advance; and jj-hp push
+        jj describe -m Log; and jj push
     else
         echo "No changes!"
     end
