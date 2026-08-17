a75df97d57c718df42d348da000f1a77d308ac7b
Russell Blickhan <rwb@rwblickhan.org>
2026-08-17 12:37:26 -0700

    Remove unused Raycast scripts

--- dot_local/share/raycast/scripts/executable_hxclip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title Edit Clipboard in Helix
-# @raycast.mode silent
-# @raycast.packageName Helix
-# @raycast.description Open clipboard content in Helix in a new Ghostty window
-
-"$HOME/.local/bin/hxclip"
