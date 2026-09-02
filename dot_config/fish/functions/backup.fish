8a21239d9490b199fe8b650d6408d9cf9403c99b
Russell Blickhan <rwb@rwblickhan.org>
2026-09-02 13:29:08 -0700

    Update backup.fish to respect new obsidian-backups location

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -12,19 +12,13 @@ function backup
     rm -rf ~/Downloads/markdown-export*
 
     echo (set_color blue)(date "+%H:%M:%S") "==> sync_bookmarks import"(set_color normal)
-    sync_bookmarks --links ~/.local/share/chezmoi/links.json --cache ~/cache.db import
+    sync_bookmarks --links ~/links.json --cache ~/cache.db import
 
     echo (set_color blue)(date "+%H:%M:%S") "==> sync_bookmarks raindrop"(set_color normal)
     sync_bookmarks --links ~/links.json --cache ~/cache.db raindrop
 
-    echo (set_color blue)(date "+%H:%M:%S") "==> cd obsidian-backups"(set_color normal)
-    cd ~/Developer/obsidian-backups
-
-    echo (set_color blue)(date "+%H:%M:%S") "==> mise run copy"(set_color normal)
-    mise run copy
-
     echo (set_color blue)(date "+%H:%M:%S") "==> backup"(set_color normal)
-    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/Personal/ ~/Music/Personal/ ~/Pictures/Personal/
+    restic -r s3:(r2b_endpoint) backup --verbose ~/Calibre\ Library/ ~/Desktop/ ~/Developer/ ~/Documents/ ~/Downloads/ ~/Movies/Personal/ ~/Music/Personal/ ~/Pictures/Personal/ ~/Documents/Obsidian\ Vaults
 
     echo (set_color green)(date "+%H:%M:%S") "==> done"(set_color normal)
 
