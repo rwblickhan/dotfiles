620b95a7234efcae08f18861b77d77f8116ab8e6
Russell Blickhan <rwb@rwblickhan.org>
2026-09-03 14:55:16 -0700

    Add more allowlist domains to goodlinks_thin

--- dot_config/fish/functions/goodlinks_thin.fish
+++ dot_config/fish/functions/goodlinks_thin.fish
@@ -345,7 +345,7 @@ function goodlinks_thin --description "Interactively thin out old read GoodLinks
     set -l base "http://localhost:9428/api/v1"
 
     # Domains that should never be offered for deletion, even if sampled.
-    set -l allowlist_domains dynomight.substack.com blog.ayjay.org v5.chriskrycho.com www.robinsloan.com www.futilitycloset.com buttondown.email/hillelwayne buttondown.com/hillelwayne twitter.com/BretDevereaux acoup.blog www.atvbt.com maya.land bsky.app/profile/bretdevereaux.bsky.social www.blackbirdspyplane.com www.hillelwayne.com www.scattered-thoughts.net www.reddit.com/r/AskHistorians borretti.me resobscura.substack.com
+    set -l allowlist_domains dynomight.substack.com blog.ayjay.org v5.chriskrycho.com www.robinsloan.com www.futilitycloset.com buttondown.email/hillelwayne buttondown.com/hillelwayne twitter.com/BretDevereaux acoup.blog www.atvbt.com maya.land bsky.app/profile/bretdevereaux.bsky.social www.blackbirdspyplane.com www.hillelwayne.com www.scattered-thoughts.net www.reddit.com/r/AskHistorians borretti.me resobscura.substack.com www.woman-of-letters.com countercraft.substack.com
 
     __goodlinks_thin_process $token $base true 50 read $allowlist_domains
     or return 1
